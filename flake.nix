b77157bf363bec879bb53b5bd2daf816cf34bdc4
spotdemo4 <me@trev.xyz>
2026-02-09 10:26:14 -0500

    chore: small style fixes

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.trev.zip/nur"
+      "https://nix.trev.zip"
     ];
     extra-trusted-public-keys = [
-      "nur:70xGHUW1+1b8FqBchldaunN//pZNVo6FKuPL4U/n844="
+      "trev:I39N/EsnHkvfmsbx8RUW+ia5dOzojTQNCTzKYij1chU="
     ];
   };
 
@@ -56,6 +56,7 @@
               # util
               bumper
               flake-release
+              renovate
             ];
           };
 
@@ -86,7 +87,7 @@
               # python
               pysentry
 
-              # nix
+              # flake
               flake-checker
 
               # actions
